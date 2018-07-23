40aa1a897538f0213e97d2bbec10e046d3da655a
Peter <peter@engineeringscotland.co.uk>
2018-07-23 14:58:25 +0100

    added lock file to stop the code from running more than one copy at a time

--- twitter-typer.py
+++ twitter-typer.py
@@ -21,44 +21,48 @@ client = UserClient(CONSUMER_KEY,
                     ACCESS_TOKEN_SECRET)
 
 os.chdir(os.path.dirname(sys.argv[0]))
-
-printHist = 'tweets.txt'
-mentionee = 'twitertypetest'
-idfile = open(printHist, 'r')
-ids = idfile.readlines()
-idfile.close()
-
-TWURL = 'http://127.0.0.1/type?'
-
-
-print('\n')
-print(str(datetime.datetime.now()))
-ret = client.api.search.tweets.get(q=mentionee,f='tweets', count = 100)
-data = ret.data.statuses
-print ('number of tweets found: ', len(data))
-idfile = open(printHist, 'a')
-for entry in data:
-    tweetid = entry['id_str'] + '\n'
-    if( not entry['user']['name'] == mentionee) and (not tweetid in ids):
-        user = entry['user']['screen_name']
-        text = emoji.demojize(entry['text'])
-        print(user, text)
-        
-        text = textwrap.wrap(text,60)
-        try:
-            f = urllib.request.urlopen(TWURL + urllib.parse.urlencode({'text': '@' + user + ' : ', 'bold' : 1, 'underline' : 1}))
+if not os.path.isfile('lock'):
+    
+    f = open('lock','w+')
+    f.close()
+    
+    printHist = 'tweets.txt'
+    mentionee = 'twitertypetest'
+    idfile = open(printHist, 'r')
+    ids = idfile.readlines()
+    idfile.close()
+    
+    TWURL = 'http://127.0.0.1/type?'
+    
+    
+    print('\n')
+    print(str(datetime.datetime.now()))
+    ret = client.api.search.tweets.get(q=mentionee,f='tweets', count = 100)
+    data = ret.data.statuses
+    print ('number of tweets found: ', len(data))
+    idfile = open(printHist, 'a')
+    for entry in data:
+        tweetid = entry['id_str'] + '\n'
+        if( not entry['user']['name'] == mentionee) and (not tweetid in ids):
+            user = entry['user']['screen_name']
+            text = emoji.demojize(entry['text'])
+            print(user, text)
             
-            multiline = False
-            for line in text:
-                if multiline:
-                    f = urllib.request.urlopen(TWURL + urllib.parse.urlencode({'text': (' ' * (len(user) + 3))}))
-                multiline = True
-                f = urllib.request.urlopen(TWURL + urllib.parse.urlencode({'text': line + '\n'}))
-            f = urllib.request.urlopen(TWURL + urllib.parse.urlencode({'text': '\n'}))
-            ids.append(tweetid)
-            idfile.write(tweetid)
-        except:
-            print('error sending tweet to typewriter')       
-idfile.close()
-        
+            text = textwrap.wrap(text,60)
+            try:
+                f = urllib.request.urlopen(TWURL + urllib.parse.urlencode({'text': '@' + user + ' : ', 'bold' : 1, 'underline' : 1}))
+                
+                multiline = False
+                for line in text:
+                    if multiline:
+                        f = urllib.request.urlopen(TWURL + urllib.parse.urlencode({'text': (' ' * (len(user) + 3))}))
+                    multiline = True
+                    f = urllib.request.urlopen(TWURL + urllib.parse.urlencode({'text': line + '\n'}))
+                f = urllib.request.urlopen(TWURL + urllib.parse.urlencode({'text': '\n'}))
+                ids.append(tweetid)
+                idfile.write(tweetid)
+            except:
+                print('error sending tweet to typewriter')       
+    idfile.close()
+    os.remove('lock')    
 
